2f97e2e8e3d85959d10c21f1e4df5840a0522398
davidYishay <daviv9@gmail.com>
2016-09-06 22:04:07 +0300

    Is palindrome
    Function for checking whether a word is a palindrome (you can write
    backwards and forwards).
    As the band: abba.

---
+++ test david yishay cohen plindrome true/test david yishay cohen plindrome true/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_david_yishay_cohen_plindrome_true
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                int halfSring; 
+            string input; 
+            Console.WriteLine(" please write some string ");
+            input = Console.ReadLine();
+
+            halfSring=  input.Length / 2;
+
+            bool pli =true;
+
+            for(int z=input.Length-1,i = 0;i<halfSring && z >halfSring && pli==true ;i++,z--)
+            {
+                if (input[i] != input[z])                
+                    pli = false;                
+            }
+
+            Console.WriteLine((pli==true ? "the sring " + input + " is plindrome" : "the string " + input + " is  not a plindrome"));
+
+        }
+    }
+}
